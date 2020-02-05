960ff769e8ea8790e5e64934a6f2712c3991ef31
tetovske <tetovske@yandex.ru>
2020-02-05 04:38:13 +0300

    work done

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,7 +19,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'service@baumanfinder.ru'
-  config.mailer = 'DeviseEmailConfirmationMailer'
+  # config.mailer = 'DeviseEmailConfirmationMailer'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
