f9213849d3fe4cfadfe1a5bf9e128a2775f894c6
tetovske <tetovske@yandex.ru>
2020-02-07 04:11:14 +0300

    hs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,14 @@ class ApplicationController < ActionController::Base
   def users_in_db
     Student.count
   end
+
+  def after_sign_in_path_for(resource)
+    # check for the class of the object to determine what type it is
+    case resource.class.to_s
+    when User.to_s
+      account_path  
+    else
+      home_path
+    end
+  end
 end
