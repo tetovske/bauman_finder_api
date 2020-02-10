d45a0854b33348dcf920aeff3eff690a859fb818
tetovske <tetovske@yandex.ru>
2020-02-11 01:24:54 +0300

    working on request handler v1

--- app/controllers/api/v1/api_request_controller.rb
+++ app/controllers/api/v1/api_request_controller.rb
@@ -1,6 +1,16 @@
-class Api::V1::ApiRequestController < ApplicationController
-  def handle_request
-    @res = params[:search_meth]
-    render :handle_request
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ApiRequestController < ApplicationController
+      include Dry::AutoInject(Container)[
+        handler_v1: 'services.request_handler_v1'
+      ]
+
+      def handle_request
+        @response = handler_v1.call(params).value_or(&:to_s)
+        render :handle_request
+      end
+    end
   end
 end
--- app/services/request_handlers/request_handler_v1.rb
+++ app/services/request_handlers/request_handler_v1.rb
@@ -1,24 +1,53 @@
-
+# frozen_string_literal: true
 
 module RequestHandlers
   class RequestHandlerV1 < Service
     include Dry::Monads[:result, :do, :maybe, :try]
     include Dry::AutoInject(Container)[
-      keys: 'services.key_keeper'
+      'services.key_keeper'
     ]
 
     def call(params = [])
+      yield setup
       yield valid_params?(params)
+
+      Success(:succeeded)
     end
 
     private
 
+    attr_accessor :keys
+
+    def setup
+      self.keys = yield key_keeper.call
+
+      Success()
+    end
+
+    # validate income params from user
     def valid_params?(params = [])
-      sup_meth = keys['search_methods'].map(&:to_sym)
+      yield validate_search_function(params)
+      yield validate_search_args(params)
+
+      Success(:validatation_succeeded)
+    end
+
+    def validate_search_function(params = [])
+      return Failure(:missing_search_function) unless params.keys.include?(keys['search_method_key_name'])
+
+      sup_meth = keys['search_methods']
+      return Failure(:invalid_search_method) unless sup_meth.include?(params[keys['search_method_key_name'].to_sym])
+
+      Success()
+    end
+
+    def validate_search_args(params = [])
+      search_args = params.reject { |k| %w[search_meth format controller action].include?(k) }
+      return Failure(:missing_search_args) if search_args.empty?
+
       sup_args = keys['search_methods_args'].map(&:to_sym)
-      return Failure(:invalid_search_method) unless sup_meth.include?(params[:search_meth].to_sym)
-      return Failure(:invalid_search_method_args) unless sup_args.include?(params[:search_meth].to_sym)
-    
+      return Failure(:invalid_search_args) unless sup_args.all? { |a| sup_args.include?(a) }
+
       Success()
     end
   end
